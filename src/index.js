690783717d2c24c81c167759a2d53ee7c1341ab0
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-12-19 21:47:29 +0530

    Added search feature using state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import reportWebVitals from './reportWebVitals';
-
-import CardList from './CardList';
 import 'tachyons';
-import { robots } from './robots';
+
+import App from './App';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
