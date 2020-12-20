68dbb2cf540fa4de769daac4353d7aa69d5e895e
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-12-20 06:15:00 +0530

    Added fetch for populating robots info and added Loading bar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+// import { robots } from "./robots";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value});
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+    // this.setState({ robots: robots });
   }
 
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-    })
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
 
-    // console.log(filteredRobots);
-    return (
-      <div className="tc">
-        <h1>Robo Friends</h1>
-        <SearchBox searchChange={this.onSearchChange}s/>
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+
+    // Loading
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      // console.log(filteredRobots);
+      return (
+        <div className="tc">
+          <h1>Robo Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
